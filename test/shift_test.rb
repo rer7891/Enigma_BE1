c1691742cc55abf7143b43746102ea992dbc02bd
Becky Robran <rerobran@gmail.com>
2019-11-01 12:27:58 -0600

    Adds test to generate keys

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -3,12 +3,21 @@ require './lib/shift.rb'
 
 class ShiftTest < Minitest::Test
 
+  def setup
+    @shift = Shift.new
+  end
+
   def test_it_exists
-    shift = Shift.new
-    assert_instance_of Shift, shift.new
+    assert_instance_of Shift, @shift
   end
 
   def test_it_can_create_a_five_digit_number
-    assert_equal "01454", shift.expects(:rand_num).returns("01454")
-  end 
+    @shift.rand_num_generator
+    #@shift.expects(:rand_num).at_least_once.returns("01454")
+  end
+
+  def test_it_can_split_up_random_number_into_keys
+    expected = {"A"=>"01", "B"=>"14", "C"=>"45", "D"=>"54"}
+    assert_equal expected, @shift.key_generator("01454")
+  end
 end
