8fa4a007f09e3003f95d4160bf62251680773a61
Becky Robran <rerobran@gmail.com>
2019-11-01 12:28:12 -0600

    Adds key generator

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,2 +1,20 @@
 class Shift
-end 
+
+  def rand_num_generator
+    '%05d' % rand(5 ** 5)
+  end
+
+  def key_generator(num = rand_num_generator)
+    hash = {}
+    hash["A"] = num[0]
+    hash["A"] += num[1]
+    hash["B"] = num[1]
+    hash["B"] += num[2]
+    hash["C"] = num[2]
+    hash["C"] += num[3]
+    hash["D"] = num[3]
+    hash["D"] += num[4]
+    hash
+    #require "pry"; binding.pry
+  end
+end
