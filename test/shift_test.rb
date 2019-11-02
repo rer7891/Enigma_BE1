a6efa12e09fe4166759fe9f69fb1c939049cea66
Becky Robran <rerobran@gmail.com>
2019-11-01 19:44:12 -0600

    "Adds new testing for mapping and generating a hash for keys"

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -12,12 +12,23 @@ class ShiftTest < Minitest::Test
   end
 
   def test_it_can_create_a_five_digit_number
-    @shift.rand_num_generator
-    #@shift.expects(:rand_num).at_least_once.returns("01454")
+    assert_equal 5, @shift.rand_num_generator.length
+    assert_equal String, @shift.rand_num_generator.class
+
   end
 
-  def test_it_can_split_up_random_number_into_keys
+  # def test_it_can_split_up_random_number_into_an_array
+  #   expected = ["01", "14", "45", "54"]
+  #   assert_equal expected, @shift.array_generator
+  # end
+
+  def test_it_can_assign_keys
+    expected = {"A"=>"0", "B"=>"0", "C"=>"0", "D"=>"0"}
+    assert_equal expected, @shift.key_generator
+  end
+
+  def test_it_can_assign_values
     expected = {"A"=>"01", "B"=>"14", "C"=>"45", "D"=>"54"}
-    assert_equal expected, @shift.key_generator("01454")
+    assert_equal expected, @shift.value_assigner
   end
 end
