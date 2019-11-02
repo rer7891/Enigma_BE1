41ba34cb24ae2b4b676460ef53d02c54ba34eb6e
Becky Robran <rerobran@gmail.com>
2019-11-02 14:46:50 -0600

    Refactors naming and method order

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,35 +1,35 @@
 class Shift
 
-  def create_a_date
-    Time.now.strftime("%d%m%y")
-  end
-
   def rand_num_generator
     '%05d' % rand(5 ** 5)
   end
 
-  def array_generator
+  def key_array_generator
     num_array = rand_num_generator.chars
     num_join = num_array.each_cons(2).map {|num| num.join}
     num_join.map {|num| num.to_i}
+  end
+
+  def create_a_date
+    Time.now.strftime("%d%m%y")
   end
 
   def multiply_date
      create_a_date.to_i * create_a_date.to_i
   end
 
-  def chop_date
+  def make_offset
     date_range = multiply_date.to_s
     date_range[-4..-1].to_i.digits
   end
 
   def combine_arrays
-    combined_array = array_generator.zip(chop_date)
+    combined_array = key_array_generator.zip(make_offset)
     combined_array.map {|num| num.sum }.flatten
   end
 
   def shift_generator
     letter_array = ["A", "B", "C", "D"]
-    x = letter_array.zip(combine_arrays).to_h
+    letter_array.zip(combine_arrays).to_h
   end
 end
