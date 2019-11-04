c5030983da36105366acd1e7ac85b41b423e4165
Becky Robran <rerobran@gmail.com>
2019-11-04 11:52:18 -0700

    Refactors encrypt message and refactors class organization

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,9 +1,11 @@
 class Encrypt
-  attr_reader :key, :offset, :encrypter
+  attr_reader :key, :offset, :encrypter, :message
 
-  def initialize(key, offset)
+  def initialize(message, key, date)
+    @message = message.downcase.chars
     @key = key
-    @offset = offset
+    @date = date.to_i
+    @offset = Offset.new(date)
   end
 
   def create_letters
@@ -11,33 +13,33 @@ class Encrypt
   end
 
   def combine_arrays
-    combined_array = @key.zip(@offset)
+    combined_array = @key.zip(@offset.make_offset)
     combined_array.map {|num| num.sum }.flatten
   end
 
   def shift_generator
-    @letter_array = ["A", "B", "C", "D"]
-    @letter_array.zip(combine_arrays).to_h
+    key_array = ["A", "B", "C", "D"]
+    key_array.zip(combine_arrays).to_h
   end
 
   def shift_hash
-      @encrypter = {}
-      @encrypter[1] = Hash[create_letters.zip(create_letters.rotate(shift_generator["A"]))]
-      @encrypter[2] = Hash[create_letters.zip(create_letters.rotate(shift_generator["B"]))]
-      @encrypter[3] = Hash[create_letters.zip(create_letters.rotate(shift_generator["C"]))]
-      @encrypter[4]= Hash[create_letters.zip(create_letters.rotate(shift_generator["D"]))]
+      key_array = [0, 1, 2, 3]
+      @encrypter = key_array.reduce({}) do |encrypter, num|
+        encrypter[num] = Hash[create_letters.zip(create_letters.rotate(combine_arrays[num]))]
+        encrypter
+      end
   end
 
-  def encrypt_message(message)
+  def encrypt_message
     shift_hash
-    num = 0
+    num = -1
 
-    message.downcase.chars.map do |letter|
+    @message.map do |letter|
       num += 1
-      if create_letters.include?(letter) && num <= 4
+      if create_letters.include?(letter) && num <= 3
         @encrypter[num][letter]
-      elsif create_letters.include?(letter) && num > 4
-        num = 1
+      elsif create_letters.include?(letter) && num > 3
+        num = 0
         @encrypter[num][letter]
       else
         letter
