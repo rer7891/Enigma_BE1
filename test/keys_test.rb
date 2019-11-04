c908593803940d29f4cc7030ff9cc90a3afa64f3
Becky Robran <rerobran@gmail.com>
2019-11-04 11:54:47 -0700

    Makes testing for key class from shift class key methods

---
+++ test/keys_test.rb
@@ -0,0 +1,24 @@
+require './test/test_helper'
+require './lib/keys'
+
+class KeysTest < Minitest::Test
+
+  def setup
+    @keys = Keys.new
+  end
+
+  def test_it_exists
+    assert_instance_of Keys, @keys
+  end
+
+  def test_it_can_create_a_five_digit_number
+    assert_equal 5, @keys.rand_num_generator.length
+    assert_equal String, @keys.rand_num_generator.class
+  end
+
+  def test_it_can_split_up_random_number_into_an_array
+    @keys.expects(:rand_num_generator).returns('01231')
+    assert_equal [1, 12, 23, 31], @keys.key_array_generator
+  end
+
+end
