3ea80624007ac1d0dde9d4880a14e570e52714bf
Becky Robran <rerobran@gmail.com>
2019-11-05 16:59:39 -0700

    Fixes typos in keys and add decrypt to runner

--- lib/runner.rb
+++ lib/runner.rb
@@ -2,9 +2,12 @@ require './lib/encrypt'
 require './lib/enigma'
 require './lib/keys'
 require './lib/offset'
+require './lib/decrypt'
 
 key = Keys.new
 offset = Offset.new
 encrypts = Encrypt.new(key, offset)
-enigma = Enigma.new(encrypts)
-puts enigma.encrypt("Im over it!", "93234")
+decrypts = Decrypt.new(key, offset)
+enigma = Enigma.new(encrypts, decrypts)
+puts enigma.encrypt("Im over it!", '34264')
+puts enigma.decrypt("sd bfvx sk!", '34264')
